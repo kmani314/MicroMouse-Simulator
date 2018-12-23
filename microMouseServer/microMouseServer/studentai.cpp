9afc67ecd273c2e1f15b049a8f02fb4e1b14f072
kmani314 <krishnamani314@gmail.com>
2018-12-23 14:23:45 -0800

    add right/left wall following

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -8,33 +8,33 @@ struct coordinateSystem {
     int ypos;
 };
 
-static struct coordinateSystem position = {0, 0}; //create a coordinate system that starts at 0,0
-static bool visited[20][20] = {{0}}; //set all squares to not visited (false)
+static struct coordinateSystem position = {0, 0}; //create a coordinate system that starts at 0,0 - This is to track which squares have been visited and which have not
+
+static bool visited[20][20] = {{0}}; // Track all squares - set all squares to not visited (false)
+
+// int orientation = 0; //Track Orientation in order to know how to increment the coordinate system
+
+vector<string> moves;
 
 void microMouseServer::studentAI() {
-    //moveForward();
-    //if (!isWallRight() && !visited[position.xpos + 1][position.ypos]) {
+    moveForward(); // we know this will work because the mouse was oriented in the last run
 
-    //} else if(!isWallForward() && !visited[position.xpos][position.ypos + 1]) {
-    //}
+   // moves.push_back("FORWARD"); //push this move to
+    printUI("FORWARD");
 
-    /*
-    * The following are the eight functions that you can call. Feel free to create your own fuctions as well.
-    * Remember that any solution that calls moveForward more than once per call of studentAI() will have points deducted.
-    *
-    *The following functions return if there is a wall in their respective directions
-    *bool isWallLeft();
-    *bool isWallRight();
-    *bool isWallForward();
-    *
-    *The following functions move the mouse. Move forward returns if the mouse was able to move forward and can be used for error checking
-    *bool moveForward();
-    *void turnLeft();
-    *void turnRight();
-    *
-    * The following functions are called when you need to output something to the UI or when you have finished the maze
-    * void foundFinish();
-    * void printUI(const char *mesg);
-    */
-
+    if (!isWallRight()) {
+        turnRight();
+     //   moves.push_back("RIGHT");
+        printUI("RIGHT");
+    } else if(!isWallForward()) {
+        // no orientation to do here, since moveForward() is called on next run
+    } else if(!isWallLeft()) {
+        turnLeft();
+       // moves.push_back("LEFT");
+        printUI("LEFT");
+    } else {
+        printUI("U-TURN");
+        turnRight();
+        turnRight();
+    }
 }
