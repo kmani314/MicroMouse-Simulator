ea15c792a769ce444d9b069f5cfd0765a19e00c1
kmani314 <krishnamani314@gmail.com>
2018-12-23 19:18:34 -0800

    scrap tile system, add tremaux marking system

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,20 +1,21 @@
 #include "micromouseserver.h"
-#include <iostream> // for debug
-
-using namespace std;
 
 struct coordinateSystem {
     int xpos;
     int ypos;
 };
 
+struct pathMarker { // my implementation of the well known pathfinder Tremaux's Algorithm - except instead of marking the beginning and end, it marks every square
+    int crossed;
+};
+
 static struct coordinateSystem position = {0, 0}; //create a coordinate system that starts at 0,0 - This is to track which squares have been visited and which have not
 
-static bool visited[20][20] = {{0}}; // Track all squares - set all squares to not visited (false)
+static pathMarker markers[20][20] = {{{0}}};
 
-int orientation = 0; //Track Orientation in order to know how to increment the coordinate system
-
-//vector<string> moves;
+static int orientation = 0; //Track Orientation in order to know how to increment the coordinate system
+static int xDir = 1; // Track how the orientation manifests into looking for blocks that have been travelled
+static int yDir = 1;
 
 int orientConstrain(int x) {
     if(x < 0) {
@@ -29,21 +30,25 @@ int orientConstrain(int x) {
 void microMouseServer::studentAI() {
     moveForward(); // we know this will work because the mouse was oriented in the last run
 
-    printUI("FORWARD");
-
     switch(orientation) {
         case(0):
             position.ypos += 1;
+            yDir = 1;
             break;
         case(1):
             position.xpos += 1;
+            xDir = 1;
             break;
         case(2):
             position.ypos -= 1;
+            yDir = -1;
             break;
         case(3):
             position.xpos -= 1;
+            xDir = -1;
     }
+
+    markers[position.xpos][position.ypos].crossed += 1;
 
     if (!isWallRight()) {
         turnRight();
@@ -51,6 +56,7 @@ void microMouseServer::studentAI() {
 
     } else if(!isWallForward()) {
         // no orientation to do here, since moveForward() is called on next run
+
     } else if(!isWallLeft()) {
         turnLeft();
        // moves.push_back("LEFT");
@@ -62,5 +68,3 @@ void microMouseServer::studentAI() {
         orientation = orientConstrain(orientation + 2);
     }
 }
-
-
