305ee2317601184a8a45400283e1ee5fd17aa4d6
kmani314 <krishnamani314@gmail.com>
2018-12-25 21:46:12 -0800

    somewhat functional - add 3/4 junction recognition and cases, add u-turn recognition, bug fixes are necessary

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -15,10 +15,16 @@ static struct coordinateSystem position = {0, 0}; // create a coordinate system 
 static int markers[20][20] = {{0}}; // path markers
 
 static int orientation = 0; // Track Orientation in order to know how to increment the coordinate system
+
+static void (microMouseServer::*turnPointer)();
+static int intent;
+static int increment;
+
+static int behind;
 static int forward;
 static int left;
 static int right;
-static int behind;
+static int possibilites[3];
 
 int orientConstrain(int x) { // ensure the orientation is between 0 and 3
     if(x < 0) {
@@ -43,23 +49,66 @@ static int yTable[3][4] = {{1, 0, -1, 0},
                            {0, 1, 0, -1}};
 
 void microMouseServer::studentAI() {
-    moveForward(); // we know this will work because the mouse was oriented in the last run
+    moveForward();
 
     position.xpos += xTable[0][orientation]; // use the lookup table to find out how to increment the x/y coords
     position.ypos += yTable[0][orientation];
 
+    turnRight();
+    behind = markers[position.xpos + xTable[1][orientConstrain(orientation + 1)]][position.ypos + yTable[1][orientConstrain(orientation + 1)]];
+    turnLeft();
+    if(isWallForward() && (isWallLeft() ^ isWallRight())) { // Are we at a turn?
 
-    if((isWallForward() && (isWallLeft() ^ isWallRight())) || !(twoOfThree(isWallLeft(), isWallRight(), isWallForward()))) { // junction / turn finding, these are the squares where decisions need to be made.
+        turnPointer = !isWallLeft() ? &microMouseServer::turnLeft : &microMouseServer::turnRight; // Generalized logic using function pointers
+        intent = !isWallLeft() ? 2 : 1;
+        increment = !isWallLeft() ? -1 : 1;
 
-        forward = isWallForward() ? markers[position.xpos + xTable[0][orientation]][position.ypos + yTable[0][orientation]] : 0; // If there is a wall, it's not worth getting the value of the marker
-        left = isWallLeft() ? markers[position.xpos + xTable[2][orientation]][position.ypos + yTable[2][orientation]] : 0;
-        right = isWallRight() ? markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]] : 0;
-        turnRight();
-        behind = isWallRight() ? markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]] : 0; // Turn around to get the value of the path we were just on
-        turnLeft();
+        if(markers[position.xpos + xTable[intent][orientation]][position.ypos + yTable[intent][orientation]] == 0) { // Is this turn unmarked?
+            (this->*turnPointer)();
+            orientation = orientConstrain(orientation + increment);
+        } else if(behind == 1) { // If not, is the path we just took only marked once?
+            turnRight();
+            turnRight();
+            orientation = orientConstrain(orientation + 2);
+        } else { // If the path we just took is marked twice and the other path has been taken once, take it.
+            (this->*turnPointer)();
+            orientation = orientConstrain(orientation + increment);
+        }
 
+    } else if(twoOfThree(!isWallLeft(), !isWallRight(), !isWallForward())) { // Are we at a 3/4 way junction?
+        forward = isWallForward() ? 0 : markers[position.xpos + xTable[0][orientation]][position.ypos + yTable[0][orientation]];
+        left = isWallLeft() ? 0 : markers[position.xpos + xTable[2][orientation]][position.ypos + yTable[2][orientation]];
+        right = isWallRight() ? 0 : markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]];
 
+        if(forward == 0 && (left == 0 && right == 0)) {
+            if(!isWallRight()) {
+                turnRight();
+                orientation = orientConstrain(orientation + 1);
+            } else if(!isWallLeft()) {
+                turnLeft();
+                orientation = orientConstrain(orientation - 1);
+            } // Otherwise don't turn because something's screwed up for sure
+
+        } else if(behind == 1) {
+            turnRight();
+            turnRight();
+            orientation = orientConstrain(orientation + 2);
+        } else {
+            // take the least taken path
+            if((right <= forward && (right <= left)) && !isWallRight()) {
+                turnRight();
+                orientation = orientConstrain(orientation + 1);
+            } else if ((left <= forward && (left <=right)) && !isWallLeft()) {
+                turnLeft();
+                orientation = orientConstrain(orientation - 1);
+            }
+        }
     } else {
         markers[position.xpos][position.ypos] += 1;
+        if(isWallLeft() && isWallRight() && isWallForward()) {
+            turnRight();
+            turnRight();
+            orientation = orientConstrain(orientation + 2);
+        }
     }
 }
