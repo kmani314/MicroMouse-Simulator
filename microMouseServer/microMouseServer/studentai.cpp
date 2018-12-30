248046ef43438cf9eb0b2749aa22745e27a3e030
kmani314 <krishnamani314@gmail.com>
2018-12-30 10:57:14 -0800

    iterative dfs works

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -14,18 +14,19 @@ struct coordinateSystem {
 
 static struct coordinateSystem position = {0, 0}; // create a coordinate system that starts at 0,0
 
-static bool markers[20][20] = {{false}}; // path markers
+static bool markers[20][20] = {{false}}; // path markers for knowing which squares have been visited
 
 static int orientation = 0; // Track Orientation in order to know how to increment the coordinate system
 
 static bool backtracking = false;
 static vector<int> moves;
 
-static int xTable[3][4] = {{0, 1, 0, -1}, {1, 0, -1, 0}, {-1, 0, 1, 0}};
+static int xTable[3][4] = {{0, 1, 0, -1}, {1, 0, -1, 0}, {-1, 0, 1, 0}}; // lookup tables for getting right / left / forward values
 
 static int yTable[3][4] = {{1, 0, -1, 0}, {0, -1, 0, 1}, {0, 1, 0, -1}};
 
-int orientConstrain(int x) {
+static int dirTable[3][3] = {{0, 2, 1}, {2, 0, 0}, {1, 0, 0}}; // lookup table for path splicing
+int orientConstrain(int x) { // constrain orientation so it is between 0 and 3 inclusive
     if(x < 0) {
         return x + 4;
     }
@@ -34,20 +35,6 @@ int orientConstrain(int x) {
     }
     return x;
 }
-int getDirection(int moveToTake, int moveTaken) {
-    int x = moveTaken - moveToTake;
-    if(x < 0) {
-        return x + 3;
-    }
-    if(x > 2) {
-        return x - 3;
-    }
-    return x;
-}
-bool twoOfThree(bool x, bool y, bool z) {
-    return x ? (y || z) : (y && z);
-}
-
 void microMouseServer::studentAI() {
     moveForward();
 
@@ -56,30 +43,32 @@ void microMouseServer::studentAI() {
 
     markers[position.xpos][position.ypos] = true;
     if(backtracking) {
-        if(!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) {
+        if(!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) { // break out of backtrack if valid path found
             backtracking = false;
             turnRight();
             orientation = orientConstrain(orientation + 1);
             int x = moves.back();
             moves.pop_back();
-            moves.push_back(getDirection(1, x));
+            moves.push_back(dirTable[1][x]); // figure out how to splice the old and new paths.
         } else if(!isWallLeft() && !markers[position.xpos + xTable[2][orientation]][position.ypos + yTable[2][orientation]]) {
             backtracking = false;
             turnLeft();
             orientation = orientConstrain(orientation - 1);
             int x = moves.back();
             moves.pop_back();
-            moves.push_back(getDirection(2, x));
+            moves.push_back(dirTable[2][x]);
         } else if(!isWallForward() && !markers[position.xpos + xTable[0][orientation]][position.ypos + yTable[0][orientation]]) {
             backtracking = false;
             int x = moves.back();
             moves.pop_back();
-            moves.push_back(getDirection(0, x));
+            moves.push_back(dirTable[0][x]);
         } else {
             switch(moves.back()) {
+            case(0):
+                break;
             case(1):
                 turnLeft();
-                orientation = orientConstrain(orientation - 1);
+                orientation = orientConstrain(orientation - 1); // if there is no valid path, keep backtracking
                 break;
             case(2):
                 turnRight();
@@ -87,13 +76,12 @@ void microMouseServer::studentAI() {
                 break;
             }
 
-            moves.pop_back();
+            moves.pop_back(); // delete the last move when backtracking
         }
-        return;
+        return; // make sure the func exits here to prevent exec of the next part
     }
 
-
-    if (!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) { // right hand wall following
+    if (!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) { // right hand wall following ensuring paths aren't visited twice
         turnRight();
         moves.push_back(1);
         orientation = orientConstrain(orientation + 1);
