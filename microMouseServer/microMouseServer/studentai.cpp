fead29c6d2294c04dd663e5527187f2a0e9d9611
kmani314 <krishnamani314@gmail.com>
2018-12-29 21:12:44 -0800

    DFS almost working, figuring out tree splicing algo

--- microMouseServer/microMouseServer/studentai.cpp
+++ microMouseServer/microMouseServer/studentai.cpp
@@ -1,9 +1,11 @@
 /* Krishna Mani - 2018
- * Implementation of the Tremaux Maze solving algorithm
- * https://github.com/kmani314/
+* DFS Iterative Graph Traversal
+* https://github.com/kmani314/
 */
 
 #include "micromouseserver.h"
+#include <iostream>
+using namespace std;
 
 struct coordinateSystem {
     int xpos;
@@ -15,6 +17,9 @@ static struct coordinateSystem position = {0, 0}; // create a coordinate system 
 static bool markers[20][20] = {{false}}; // path markers
 
 static int orientation = 0; // Track Orientation in order to know how to increment the coordinate system
+
+static bool backtracking = false;
+static vector<int> moves;
 
 static int xTable[3][4] = {{0, 1, 0, -1}, {1, 0, -1, 0}, {-1, 0, 1, 0}};
 
@@ -29,7 +34,16 @@ int orientConstrain(int x) {
     }
     return x;
 }
-
+int getDirection(int moveToTake, int moveTaken) {
+    int x = moveTaken - moveToTake;
+    if(x < 0) {
+        return x + 3;
+    }
+    if(x > 2) {
+        return x - 3;
+    }
+    return x;
+}
 bool twoOfThree(bool x, bool y, bool z) {
     return x ? (y || z) : (y && z);
 }
@@ -39,17 +53,61 @@ void microMouseServer::studentAI() {
 
     position.xpos += xTable[0][orientation];
     position.ypos += yTable[0][orientation];
-    // implement dfs. go right until a dead end, go back and go left, repeat.
-    markers[position.xpos][position.ypos] = true;
 
-    if (!isWallRight()) { // right hand wall following
+    markers[position.xpos][position.ypos] = true;
+    if(backtracking) {
+        if(!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) {
+            backtracking = false;
+            turnRight();
+            orientation = orientConstrain(orientation + 1);
+            int x = moves.back();
+            moves.pop_back();
+            moves.push_back(getDirection(1, x));
+        } else if(!isWallLeft() && !markers[position.xpos + xTable[2][orientation]][position.ypos + yTable[2][orientation]]) {
+            backtracking = false;
+            turnLeft();
+            orientation = orientConstrain(orientation - 1);
+            int x = moves.back();
+            moves.pop_back();
+            moves.push_back(getDirection(2, x));
+        } else if(!isWallForward() && !markers[position.xpos + xTable[0][orientation]][position.ypos + yTable[0][orientation]]) {
+            backtracking = false;
+            int x = moves.back();
+            moves.pop_back();
+            moves.push_back(getDirection(0, x));
+        } else {
+            switch(moves.back()) {
+            case(1):
+                turnLeft();
+                orientation = orientConstrain(orientation - 1);
+                break;
+            case(2):
+                turnRight();
+                orientation = orientConstrain(orientation + 1);
+                break;
+            }
+
+            moves.pop_back();
+        }
+        return;
+    }
+
+
+    if (!isWallRight() && !markers[position.xpos + xTable[1][orientation]][position.ypos + yTable[1][orientation]]) { // right hand wall following
         turnRight();
-    } else if (!isWallForward()) {
+        moves.push_back(1);
+        orientation = orientConstrain(orientation + 1);
+    } else if (!isWallForward() && !markers[position.xpos + xTable[0][orientation]][position.ypos + yTable[0][orientation]]) {
         // go forward
-    } else if (!isWallLeft()) {
+        moves.push_back(0);
+    } else if (!isWallLeft() && !markers[position.xpos + xTable[2][orientation]][position.ypos + yTable[2][orientation]]) {
         turnLeft();
+        orientation = orientConstrain(orientation - 1);
+        moves.push_back(2);
     } else {
         turnRight(); // Backtrack if dead end hit
         turnRight();
+        backtracking = true;
+        orientation = orientConstrain(orientation + 2);
     }
 }
